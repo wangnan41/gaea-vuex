c53aba0fbea9f3cbc072d5a8910776d7825a8f9e
wangnan <wangnan7@jd.com>
2018-07-24 14:22:11 +0800

    gaea-vuex init

---
+++ src/component/a.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="wrapp">
+		<h1>Vuex demo</h1>
+		<p>a:{{aNum}}</p>
+		<p>3倍增加后的a:{{aNum}}</p>
+		<input  type="button" value="增加"  @click="addSync(5)"/>
+	</div>
+</template>
+
+<script>
+	import {mapState, mapActions,mapMutations} from 'vuex';
+	export default {
+		data(){
+	        return{
+	        }
+	    },
+		computed:mapState({
+				aNum:state=>state.a.num, 
+			}),
+		methods:{
+			...mapActions({
+				addSync:'a/increase' //this.addSync 映射 this.$store.dispatch('a/increase')
+			}),
+			...mapMutations({
+				add:'a/INCREASE'//this.add 映射 this.$store.commit('a/INCREASE')
+			})
+		},
+		created(){
+
+		},
+		mounted(){
+			this.add(3);
+			
+		},
+		destroyed(){
+
+		}
+	}
+	
+</script>
+
+<style lang="scss" >
+	.wrapp{
+		font-size:14px;
+	}
+	input{
+		background: #ccc;
+	}
+	
+</style>
---
+++ src/view/index.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="wrapper">
+        <div class="box">我是22首页</div>
+        <div class="box1">
+            <span>测试autoprefixer补齐问题</span>
+            <span>测试autoprefixer补齐问题</span>
+        </div>
+        <router-link to="/detail"><h3>去详情页1</h3></router-link>
+        <router-link to="/detail2"><h3>去详情页2（懒加载）</h3></router-link>
+
+        <h3>我是vuex的a组件</h3>
+        <aVuex />
+    </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+import Qs from 'qs';
+import  aVuex from '../component/a.vue';
+
+export default {
+    data(){
+        return{
+        }
+    },
+    components: {
+        aVuex
+    },
+    methods:{
+        query(){
+            // axios.post('/kd/index',Qs.stringify({})).then((response) => {
+            //         let result = response.data;
+            //         if(result.success) {
+
+            //             succ && succ(result);
+            //         } else {
+                        
+            //             err && err(result);
+            //         }
+            //     })
+            //     .catch((error) => {
+
+            //        // err && err();
+            //     });
+        },
+        test(){
+            let obj = {
+                name:'wang',
+                age:'19',
+            }    
+            let obj2 = Object.assign({},obj);
+        }
+    },
+    mounted(){
+        this.query();
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper{
+    .box{
+        font-size:.5rem;
+    }
+    h3{
+        font-size:.3rem;
+    }
+    .box1{
+        display: flex;
+        transform:rotate(7deg);
+    }
+    //图片引用
+    //background:url('../asset/img/loading.gif');
+}
+</style>
