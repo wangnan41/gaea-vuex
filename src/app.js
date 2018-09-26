113f95a1f37c7c898d2f721ff344f8b157a55c41
wangnan <wangnan7@jd.com>
2018-09-26 18:01:00 +0800

    update

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import App from './app.vue';
 import router from './router.js';
+import store from './store';
+
 //import 'babel-polyfill' 
 //如需兼容低端浏览器，请打开此注释（如果是对外的项目，不建议开启，比较大70kb。可以自己安装babel针对你的es6的写法
 //加入了for of 和 Object.assign 常用的两个转换 ）
@@ -12,6 +14,7 @@ Vue.use(Loading);
 
 const app = new Vue({
   router,
+  store,
   render: h => h(App),
   mounted () {
     // You'll need this for renderAfterDocumentEvent.
